fccbe215bed3d88cf8e3cb31178dc519da4c0223
Michał Sadowski <michal.sadowski@roche.com>
2023-06-20 14:26:52 +0200

    Loading animation for login button

--- una-core/components/login/Login.tsx
+++ una-core/components/login/Login.tsx
@@ -1,37 +1,56 @@
-'use client';
+"use client";
 
-import { FaBattleNet } from 'react-icons/fa';
-import { signIn } from 'next-auth/react';
+import { FaBattleNet } from "react-icons/fa";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 const Login = () => {
-    return (
-        <button
-            className="
+  const [isLogging, setIsLogging] = useState(false);
+
+  return (
+    <button
+      className={`                  
                 flex
                 flex-row
                 items-center
                 justify-between
                 gap-3
                 bg-blue-700
-                hover:bg-blue-800
+                ${isLogging ? `bg-blue-800` : `hover:bg-blue-800`}
                 text-white font-semibold
                 md:py-4
                 md:px-8
                 rounded-full
                 focus:outline-none
                 focus:shadow-outline
-                text-xl
-                "
-            onClick={() => signIn('battlenet')}    
-        >
-            <FaBattleNet size="64" >
+                text-xl`}
+      onClick={() => {
+        setIsLogging(true);
+        signIn("battlenet");
+      }}
+      disabled={isLogging}
+    >
+      <FaBattleNet size="64"></FaBattleNet>
+      {!isLogging && (
+        <div className="hidden md:block">Login using Battle.net!</div>
+      )}
 
-            </FaBattleNet>
-            <div className='hidden md:block'>
-                Login using Battle.net!
-            </div>
-        </button>
-    );
-}
+      {isLogging && (
+        <div className="flex flex-row items-center justify-between animate-[pulse_1.25s_linear_infinite] space-x-6">
+          {[...Array(3)].map((_) => (
+            <div
+              className="
+                        bg-white
+                        rounded-full
+                        h-4
+                        w-4 
+                "
+            ></div>
+          ))}
+        </div>
+      )}
+    </button>
+  );
+};
 
 export default Login;
--- una-core/pages/api/auth/[...nextauth].ts
+++ una-core/pages/api/auth/[...nextauth].ts
@@ -1,112 +1,125 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { Account, AuthOptions } from "next-auth";
 import BattleNetProvider from "next-auth/providers/battlenet";
 import axios from "axios";
 
 type WowProfileResponse = {
-    wow_accounts: WoWAccountsResponse[]
-}
+  wow_accounts: WoWAccountsResponse[];
+};
 
 type WoWAccountsResponse = {
-    id: number,
-    characters: WoWCharacterResponse[]
-}
+  id: number;
+  characters: WoWCharacterResponse[];
+};
 
 type WoWCharacterResponse = {
-    id: number
-}
+  id: number;
+};
 
 type WoWGuildRosterMemberResponse = {
-    character: WoWCharacterResponse,
-    rank: number
-}
+  character: WoWCharacterResponse;
+  rank: number;
+};
 
 type WoWGuildRosterResponse = {
-    members: WoWGuildRosterMemberResponse[]
-}
+  members: WoWGuildRosterMemberResponse[];
+};
 
 export const authOptions: AuthOptions = {
-    providers: [
-        BattleNetProvider({
-            issuer: "https://eu.battle.net/oauth",
-            authorization: { params: { scope: "openid wow.profile" } },
-        }),
-        // maybe additional mail
-    ],
-    pages: {
-        signIn: '/',
-    },
-    debug: process.env.NODE_ENV === 'development',
-    session: {
-        strategy: "jwt",
-    },
-    //secret: process.env.AUTH_SECRET,
-    secret: 'say_lalisa_love_me_lalisa_love_me_hey',
-    callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            const token = account?.access_token;
-            console.log(`${token}`)
+  providers: [
+    BattleNetProvider({
+      issuer: "https://eu.battle.net/oauth",
+      authorization: { params: { scope: "openid wow.profile" } },
+      clientId: process.env.APPLICATION_WOW_ID,
+      clientSecret: process.env.APPLICATION_WOW_SECRET
+    }),
+    // maybe additional mail
+  ],
+  pages: {
+    signIn: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  //secret: process.env.AUTH_SECRET,
+  secret: "say_lalisa_love_me_lalisa_love_me_hey",
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const token = account?.access_token;
+      console.log(`${token}`);
 
-            if (token) {
-                return isProfileInGuild(token);
-            } else {
-                // Return false to display a default error message
-                return false
-                // Or you can return a URL to redirect to:
-                // return '/unauthorized'
-            }
+      if (token) {
+        const result = await isProfileInGuild(token);
+
+        if (result) {
+          account.scope = `${result.rank}`;
+          return true;
         }
-    }
-}
+      }
+
+      return false;
+    },
+    async jwt({ token, account, profile }) {
+        // Persist the OAuth access_token and or the user id to the token right after signin
+        if (account) {
+          token.accessToken = account.access_token;
+          token.scope = account.scope;
+          console.log(`Token scopes ${token.scope}`);
+        }
+        return token
+      }
+  },
+};
 
 async function isProfileInGuild(token: string) {
-    try {
-        // 👇️ const data: GetUsersResponse
-        const { data, status } = await axios.get<WowProfileResponse>(
-            `https://eu.api.blizzard.com/profile/user/wow?namespace=profile-eu&locale=en_GB&access_token=${token}`,
-            {
-                headers: {
-                    Accept: 'application/json',
-                },
-            },
-        );
+  try {
+    const { data, status } = await axios.get<WowProfileResponse>(
+      `https://eu.api.blizzard.com/profile/user/wow?namespace=profile-eu&locale=en_GB&access_token=${token}`,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
 
-        const wowGuildResponse = await axios.get<WoWGuildRosterResponse>(
-            `https://eu.api.blizzard.com/data/wow/guild/magtheridon/una-familia/roster?namespace=profile-eu&locale=en_GB&access_token=${token}`,
-            {
-                headers: {
-                    Accept: 'application/json',
-                },
-            },
-        );
+    const wowGuildResponse = await axios.get<WoWGuildRosterResponse>(
+      `https://eu.api.blizzard.com/data/wow/guild/magtheridon/una-familia/roster?namespace=profile-eu&locale=en_GB&access_token=${token}`,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
 
-        //All characters ID array
-        const characterIds = data.wow_accounts.flatMap((account) => account.characters).flatMap((child) => child.id);
+    //All characters ID array
+    const characterIds = data.wow_accounts
+      .flatMap((account) => account.characters)
+      .flatMap((child) => child.id);
 
-        console.log(wowGuildResponse.data);
-
-        //Check if a character exists in the Guild
-        const character = wowGuildResponse.data.members
-            .filter((member) => characterIds.includes(member.character.id))
-            .reduce((minRank, character) => {
-                if (!minRank.rank || (character.rank !== undefined && character.rank < minRank.rank)) {
-                    return character;
-                }
-
-                return minRank;
-            });
-
-        console.log(character);
-
-        return true;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.log('error message: ', error.message);
-            return false;
-        } else {
-            console.log('unexpected error: ', error);
-            return false;
+    //Check if a character exists in the Guild
+    const character = wowGuildResponse.data.members
+      .filter((member) => characterIds.includes(member.character.id))
+      .reduce((minRank, character) => {
+        if (
+          !minRank.rank ||
+          (character.rank !== undefined && character.rank < minRank.rank)
+        ) {
+          return character;
         }
+
+        return minRank;
+      });
+
+    return character;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return false;
+    } else {
+      console.log("unexpected error: ", error);
+      return false;
     }
+  }
 }
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
